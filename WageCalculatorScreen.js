921fcf65485d38b06e96ad17007261861f86a6d3
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-09-18 13:32:07 -0600

    implemented AsyncStorage. wage calculator now saves an hourly wage

--- WageCalculatorScreen.js
+++ WageCalculatorScreen.js
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function WageCalculatorScreen() {
   const [hourlyRate, setHourlyRate] = useState(""); // State to store hourly rate
   const [selectedDays, setSelectedDays] = useState([]); // State to store selected days
+  const [totalEarnings, setTotalEarnings] = useState(""); // State to store total earnings
+
+  useEffect(() => {
+    // Load the hourly rate from AsyncStorage when the component mounts
+    loadHourlyRate();
+  }, []);
+
+  // Function to load hourly rate from AsyncStorage
+  const loadHourlyRate = async () => {
+    try {
+      const savedHourlyRate = await AsyncStorage.getItem("hourlyRate");
+      if (savedHourlyRate) {
+        setHourlyRate(savedHourlyRate);
+      }
+    } catch (error) {
+      console.error("Error loading hourly rate:", error);
+    }
+  };
+
+  // Function to save the hourly rate
+  const saveHourlyRate = async () => {
+    try {
+      await AsyncStorage.setItem("hourlyRate", hourlyRate);
+      alert("Hourly rate saved successfully.");
+    } catch (error) {
+      console.error("Error saving hourly rate:", error);
+    }
+  };
 
   // Function to calculate total earnings
   const calculateTotalEarnings = () => {
     // Implement the logic to calculate earnings here
     // You can use hourlyRate and selectedDays state to perform calculations
     // Update the total earnings in state
+    // For now, let's simply set a placeholder value
+    setTotalEarnings("Calculating..."); // Replace this with your calculation logic
   };
 
   return (
@@ -22,9 +53,10 @@ export default function WageCalculatorScreen() {
         value={hourlyRate}
         keyboardType="numeric"
       />
+      <Button title="Save Hourly Rate" onPress={saveHourlyRate} />
       {/* Add UI elements to select days */}
       <Button title="Calculate Earnings" onPress={calculateTotalEarnings} />
-      <Text>Total Earnings: {/* Display the calculated earnings here */}</Text>
+      <Text>Total Earnings: {totalEarnings}</Text>
     </View>
   );
 }
